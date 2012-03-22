8310321e5c6f92d777df9ab7de440aba3786c121
Shamun <shamun.toha@gmail.com>
2012-03-23 00:59:51 +0100

    Update

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: root
+ *
+ * Created on March 23, 2012, 12:53 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return (EXIT_SUCCESS);
+}
+
